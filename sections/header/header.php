8daff74c4d23ae7adfcdc7015117e9987b0959d6
Daniil Yurkevich <yurkevich.crocode@gmail.com>
2024-04-15 23:02:44 +0300

    inital commit

---
+++ sections/header/header.php
@@ -0,0 +1,11 @@
+<header class="header">
+    <div class="container">
+        <nav class="header__wrapper">
+            <ul>
+                <li><a href="#">Главная</a></li>
+                <li><a href="#">Каталог</a></li>
+                <li><a href="#">Доставка и оплата</a></li>
+            </ul>
+        </nav>
+    </div>
+</header>
